20a59bd8394302de2054504bb3a3fc48ff6f3587
Administrator <admin@example.com>
2020-11-03 14:13:08 +0300

    +change_color_pygame

--- change_colot(pygame).py
+++ change_colot(pygame).py
@@ -6,13 +6,12 @@
 import pygame
 
 
-
 def point_to_circle(point, circle):
     a = point[0] - circle[0]
     b = point[1] - circle[1]
     r = circle[2]
 
-    return a**2 + b**2 <= r**2
+    return a ** 2 + b ** 2 <= r ** 2
 
 
 def point_to_rect(point, rect):
@@ -32,28 +31,26 @@ def circle_to_circle(circle1, circle2):
     b = circle1[1] - circle2[1]
     c = circle1[2] + circle2[2]
 
-    return a**2 + b**2 <= c**2
+    return a ** 2 + b ** 2 <= c ** 2
+
 
 pygame.init()
-
 
 SIZE = (1200, 800)
 TITLE = "Test_color"
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption(TITLE)
 
-
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-GREEN = (0,255,0)
-BLUE = (0,0, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 circle = [600, 400, 100]
 rectangle = [500, 300, 200, 200]
 
-
-case = 2
+case = 3
 
 done = False
 
@@ -81,7 +78,6 @@ while not done:
         else:
             color = WHITE
 
-
     screen.fill(BLACK)
 
     if case == 1:
@@ -90,7 +86,7 @@ while not done:
         pygame.draw.rect(screen, color, [rectangle[0], rectangle[1], rectangle[2], rectangle[3]])
     elif case == 3:
         pygame.draw.circle(screen, color, [circle[0], circle[1]], circle[2])
-        pygame.draw.circle(screen, WHITE, [circle2[0], circle2[1]], circle2[2])
+        pygame.draw.circle(screen, BLUE, [circle2[0], circle2[1]], circle2[2])
 
     pygame.display.flip()
 
